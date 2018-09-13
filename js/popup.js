886f075a25c1f1cedb3c311ebd1c8ad78ad13054
Dimahtml <42168781+Dimahtml@users.noreply.github.com>
2018-09-13 17:34:55 +0500

    исправил мелкие замечания

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,7 @@
-			var button = document.querySelector(".search");
-			var popup = document.querySelector(".search-form");
-			popup.classList.add("modal-no-show");	
-			button.addEventListener("click", function (evt) {
-				evt.preventDefault();
-				popup.classList.toggle("modal-no-show");	
-			});
+		var button = document.querySelector(".search");
+		var popup = document.querySelector(".search-form");
+		popup.classList.add("modal-no-show");	
+		button.addEventListener("click", function (evt) {
+			evt.preventDefault();
+			popup.classList.toggle("modal-no-show");	
+		});
